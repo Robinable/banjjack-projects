d5a31bfdf4b8bd32eb4dd00f6d49a57c7fce1350
LSJ <isj19785@gmail.com>
2022-11-11 00:42:15 +0900

    인터셉트 완료?

--- src/main/java/com/green/controller/LoginController.java
+++ src/main/java/com/green/controller/LoginController.java
@@ -2,22 +2,17 @@ package com.green.controller;
 
 import com.green.service.UserService;
 import com.green.vo.UserVo;
-import org.junit.runner.Request;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpRequest;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.ModelAndView;
-import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 
-import javax.print.attribute.standard.JobOriginatingUserName;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
-import javax.sound.midi.Soundbank;
-import java.io.IOException;
 import java.util.HashMap;
 
 
@@ -78,7 +73,7 @@ public class LoginController {
                              @RequestParam("userpassword") String userpassword,
                              HttpSession session,
                              HttpServletResponse response,
-                             Model model) throws IOException {
+                             Model model) {
 
         System.out.println(username);
         System.out.println(userpassword);
--- src/main/java/com/green/interceptor/AuthInterceptor.java
+++ src/main/java/com/green/interceptor/AuthInterceptor.java
@@ -1,13 +1,17 @@
 package com.green.interceptor;
 
 import com.green.vo.UserVo;
+import lombok.extern.java.Log;
+import lombok.extern.log4j.Log4j;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.util.ObjectUtils;
 import org.springframework.web.servlet.HandlerInterceptor;
 import org.springframework.web.servlet.ModelAndView;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
-
+@Log
 public class AuthInterceptor implements HandlerInterceptor {
     // preHandle
     // 컨트롤러로 request 들어가기 전에 수행함
@@ -19,31 +23,28 @@ public class AuthInterceptor implements HandlerInterceptor {
 
         // session 객체 가져옴
         // 컨트롤러에서 만든 vo를 담은 세션을 불러와서 변수에 저장
+        log.info("테스트");
         HttpSession session = request.getSession();
         UserVo userVo = (UserVo) session.getAttribute("login");
         System.out.println("preHaondle: " + userVo);
-//        if(ObjectUtils.isEmpty(userVo.get_id())) {
-//            // vo를 담은 login 세션이 비어있다면 == 로그인 안되어있음
-//            // 로그인이 되어있지 않다면 /login으로 이동
-//
-//           // response.sendRedirect("/login");
-////            return false;                         // 로그인페이지로 넘어갔기 때문에 컨트롤러 요청 더 받지않음
-//        } else {
-//            session.setMaxInactiveInterval(30*60);
-////            return true;
-//        }
-        return true;
-        // request.getRequestURL(): 요청된 URL 전체 주소 정보 반환
-        // requsetUrl:전체주소 정보가 /login이면 페이지 이동
-//        String requsetUrl = request.getRequestURL().toString();
-//        if(requsetUrl.contains("/login")) {
-//            return true;
-//        }
+        if(userVo == null) {
+            // vo를 담은 login 세션이 비어있다면 == 로그인 안되어있음
+            // 로그인이 되어있지 않다면 /login으로 이동
 
-//        return HandlerInterceptor.super.preHandle(request, response, handler);
-        // true: 컨트롤러 실행-페이지 넘어감 false:컨트롤러 실행x-페이지 안 넘어감
+            response.sendRedirect("/login");
+            return false;                         // 로그인페이지로 넘어갔기 때문에 컨트롤러 요청 더 받지않음
+        } else {
+            session.setMaxInactiveInterval(30*60);
+            return true;
+        }
+
+//        return true;
+
+        //return HandlerInterceptor.super.preHandle(request, response, handler);
+        //true: 컨트롤러 실행-페이지 넘어감 false:컨트롤러 실행x-페이지 안 넘어감
 
     }
+
     
     // postHandle
     // 컨트롤러 실행 후, 뷰 실행 전에 수행함
@@ -52,17 +53,15 @@ public class AuthInterceptor implements HandlerInterceptor {
             HttpServletRequest request,
             HttpServletResponse response,
             Object handler,
-            ModelAndView modelAndView) throws Exception {
+            ModelAndView mv) throws Exception {
 
+        System.out.println(request.getRequestURL());
         HttpSession session = request.getSession();
         UserVo userVo = (UserVo) session.getAttribute("login");
         System.out.println("postHaondle: " + userVo);
-        Object userInfo = modelAndView.addObject("vo", userVo);
+        mv.addObject("vo", userVo);
 
-
-
-
-        HandlerInterceptor.super.postHandle(request, response, handler, modelAndView);
+        HandlerInterceptor.super.postHandle(request, response, handler, mv);
 
     }
     
